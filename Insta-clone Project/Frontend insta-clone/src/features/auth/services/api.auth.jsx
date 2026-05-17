9786791f6af1f38ff4a47dc937b9487cfcc654ec
vijaym105 <malusarevijay10@gmail.com>
2026-05-17 14:59:41 +0530

    Render URL settled

--- Insta-clone Project/Frontend insta-clone/src/features/auth/services/api.auth.jsx
+++ Insta-clone Project/Frontend insta-clone/src/features/auth/services/api.auth.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/auth",
+    baseURL: "http://localhost:3000/api/auth"  && import.meta.env.VITE_API_URL + "/api/auth",
     withCredentials: true
 })
 
--- Insta-clone Project/Frontend insta-clone/src/features/post/services/post.api.js
+++ Insta-clone Project/Frontend insta-clone/src/features/post/services/post.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3000" && import.meta.env.VITE_API_URL,
     withCredentials: true
 })
 
