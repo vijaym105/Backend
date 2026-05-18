926382d8db14f0c6542512cba1197b983fd1a239
vijaym105 <malusarevijay10@gmail.com>
2026-05-18 13:32:38 +0530

    final push insta-clone

--- Insta-clone Project/Backend insta-clone/controller/auth.controller.js
+++ Insta-clone Project/Backend insta-clone/controller/auth.controller.js
@@ -1,111 +1,111 @@
 const userModel = require('../models/user.model')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
- async function registerController (req, res) {
-    try{
-    const {username, email, password, bio, profilePic} = req.body
+async function registerController(req, res) {
+    try {
+        const { username, email, password, bio, profilePic } = req.body
 
-    const isUserExist = await userModel.findOne({
-        $or : [
-           { username},
-           { email}
-        ]
-    })
-    
-    if(isUserExist){
-        return  res.status(409).json({
-            message: isUserExist.email == email ? 
-            "Email already in use" : "Username already in use"
+        const isUserExist = await userModel.findOne({
+            $or: [
+                { username },
+                { email }
+            ]
         })
-    }
-    
-    const hash = await bcrypt.hash(password, 10)
 
-    const user = await userModel.create({
-        username,
-        email,
-        password:hash,
-        bio,
-        profilePic
-    })
-    const token = jwt.sign({
-        id: user._id , username: user.username
-    }, process.env.JWT_SECRET)
-        
-    res.cookie("token", token, {
-   httpOnly: true,
-   secure: true,
-   sameSite: "none"
-})
-
-    res.status(201).json({
-        message:"User account created successfuly",
-        success:true,
-        user:{
-            username:user.username,
-            email:user.email,
-            bio:user.bio,
-            profilePic: user.profilePic
+        if (isUserExist) {
+            return res.status(409).json({
+                message: isUserExist.email == email ?
+                    "Email already in use" : "Username already in use"
+            })
         }
-    })
-    if(!user){
-    return res.status(404).json({
-        message: "Something wrong from client side",
-        success: false
-    })
-}
-}
-catch(error){
-    console.log(error)
-    res.status(500).json({
-        message: "Problem from server side"
-    })
-}
-}
 
-async function loginController (req,res) {
-    try{
-    const {username, email, password} = req.body
+        const hash = await bcrypt.hash(password, 10)
 
-    const user = await userModel.findOne({
-        $or: [
-            {username},
-            {email}
-        ]
-    }).select("+password")
-    if(!user){
-        return res.status(404).json({
-            message:"user not found"
+        const user = await userModel.create({
+            username,
+            email,
+            password: hash,
+            bio,
+            profilePic
+        })
+        const token = jwt.sign({
+            id: user._id, username: user.username
+        }, process.env.JWT_SECRET)
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        })
+
+        res.status(201).json({
+            message: "User account created successfuly",
+            success: true,
+            user: {
+                username: user.username,
+                email: user.email,
+                bio: user.bio,
+                profilePic: user.profilePic
+            }
+        })
+        if (!user) {
+            return res.status(404).json({
+                message: "Something wrong from client side",
+                success: false
+            })
+        }
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Problem from server side"
         })
     }
-   
-    const isPassValid = await bcrypt.compare(password, user.password)
-    
-    if(!isPassValid){
-        return res.status(401).json({
-            message:"Incorrect password"
-        })
-    }
-    const token = jwt.sign({
-        id: user._id , username: user.username
-    }, process.env.JWT_SECRET,
-        {expiresIn:"1d"}
-    )
-    res.cookie("token",token)
-
-    res.status(200).json({
-        message:"User loged in successfuly"
-    })
-}catch(error){
-    console.log(error)
 }
+
+async function loginController(req, res) {
+    try {
+        const { username, email, password } = req.body
+
+        const user = await userModel.findOne({
+            $or: [
+                { username },
+                { email }
+            ]
+        }).select("+password")
+        if (!user) {
+            return res.status(404).json({
+                message: "user not found"
+            })
+        }
+
+        const isPassValid = await bcrypt.compare(password, user.password)
+
+        if (!isPassValid) {
+            return res.status(401).json({
+                message: "Incorrect password"
+            })
+        }
+        const token = jwt.sign({
+            id: user._id, username: user.username
+        }, process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        )
+        res.cookie("token", token)
+
+        res.status(200).json({
+            message: "User loged in successfuly"
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 async function getMeController(req, res) {
     const userId = req.user.id
     const user = await userModel.findById(userId)
 
-    if(!user){
+    if (!user) {
         return res.status(404).json({
             message: "User not found please send token through login api"
         })
