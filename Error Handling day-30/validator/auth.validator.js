3240d58766b8d7cbde8f6795b34f680cc3bb17f9
vijaym105 <malusarevijay10@gmail.com>
2026-06-14 15:55:58 +0530

    pract

--- Error Handling day-30/validator/auth.validator.js
+++ Error Handling day-30/validator/auth.validator.js
@@ -1,9 +1,10 @@
 import {body, validationResult} from 'express-validator'
 
 
-const validate = (res , req , next) => {
+const validate = (req, res , next) => {
+    
     const errors = validationResult(req)
-
+    
     if(errors.isEmpty()){
         return next()
     }
@@ -13,19 +14,18 @@ const validate = (res , req , next) => {
     })
 }
 
-export const regValidation = [
-    body("username").isString().withMessage("username should be string"),
-    body("email").isEmail().withMessage("email address is not valid"),
-    body("password").custom((value) => {
-        if(value.length < 6){
-            throw new error("password should not be more than 6 characters")
+const regValidation = [
+    body("username").isString().withMessage("username must be in string format"),
+    body("email").isEmail().withMessage("Use valid gmail address"),
+    body("password").custom((val) => {
+        if(val.length < 6){
+            throw new error("password should be more or equal to 6 characters")
         }
-         const passwordRegex = /^(?=.*[A-Z])(?=.*\d).+$/
-
-        if(!passwordRegex.test(value)){
-            throw new error("password should contain at least one uppercase letter and one number")
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d).+$/
+        if(passwordRegex.test(val)){
+            throw new error("Password should contain at least 1 captital letter and 1 number")
         }
         return true
-    }).withMessage("password should be at least 6 characters long and contain at least one uppercase letter and one number"),
+    }).withMessage("Password should be 6 words and should contain at least 1 Capital letter and 1 number."),
     validate
 ]
