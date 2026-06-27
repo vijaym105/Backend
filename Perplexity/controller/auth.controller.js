a2ce4a080ed36e5e7001295937291d5c61056f5b
vijaym105 <malusarevijay10@gmail.com>
2026-06-27 15:59:25 +0530

    Models and controller

--- Perplexity/controller/auth.controller.js
+++ Perplexity/controller/auth.controller.js
@@ -1,7 +1,33 @@
 import userModel from '../models/user.model.js';
 
 export const register = async (req, res) => {
+    
+    const {username, email, password} = req.body
 
+    const isUserExists = await userModel.findOne({
+        $or: [{username}, {email}]
+    })
 
+    if(isUserExists) {
+        return res.status(400).json({
+            message:  `Username or email already exists`,
+            success: false
+        })
+    }
+
+    const user = await userModel.create({
+        username,
+        email,
+        password
+    })
+    
+    res.status(201).json({
+        message: 'User created successfully',
+        success: true,
+        user:{
+            username: user.username,
+            email: user.email
+        }
+    })
 }
 
--- Perplexity/src/app.js
+++ Perplexity/src/app.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
-
-
-
+import authRouter from '../routes/auth.routes.js'
 const app = express()
-
 app.use(express.json())
+
+app.use('/api/auth', authRouter)
+
 app.use(cookieParser())
 
 export default app
