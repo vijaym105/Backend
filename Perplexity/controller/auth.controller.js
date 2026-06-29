c56798a37f8e1072f3d99d52b6c5c73278b7cd01
vijaym105 <malusarevijay10@gmail.com>
2026-06-29 15:48:16 +0530

    nodeMailer

--- Perplexity/controller/auth.controller.js
+++ Perplexity/controller/auth.controller.js
@@ -1,16 +1,17 @@
 import userModel from '../models/user.model.js';
+import { sendEmail } from '../services/mail.service.js';
 
 export const register = async (req, res) => {
-    
-    const {username, email, password} = req.body
+
+    const { username, email, password } = req.body
 
     const isUserExists = await userModel.findOne({
-        $or: [{username}, {email}]
+        $or: [{ username }, { email }]
     })
 
-    if(isUserExists) {
+    if (isUserExists) {
         return res.status(400).json({
-            message:  `Username or email already exists`,
+            message: `Username or email already exists`,
             success: false
         })
     }
@@ -20,11 +21,15 @@ export const register = async (req, res) => {
         email,
         password
     })
-    
+
+    await sendEmail(email, 'Welcome to Perplexity', 
+        `Hello ${username},\n\nThank you for registering with Perplexity! We're excited to have you on board.\n\nBest regards,\nThe Perplexity Team`);
+
     res.status(201).json({
         message: 'User created successfully',
         success: true,
-        user:{
+        user: {
+            id: user._id,
             username: user.username,
             email: user.email
         }
---
+++ Perplexity/services/mail.service.js
@@ -0,0 +1,33 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: process.env.GOOGLE_USER,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        refreshToken: process.env.REFRESH_TOKEN_SECRET
+    }
+})
+
+transporter.verify()
+    .then(() => {
+        console.log("Ready to send emails");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+export const sendEmail = async (to, subject, text) => {
+    const mailOptions = {
+        from: process.env.GOOGLE_USER,
+        to,
+        subject,
+        text
+    }
+
+    const res = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + res.response);
+}
+
